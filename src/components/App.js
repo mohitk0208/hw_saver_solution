b9344c23b295ea9e0ba9aa6aab7723410318249f
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2021-03-05 02:05:45 +0530

    add private route

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,18 @@ import AuthProvider from "../contexts/AuthContext";
 import Login from "./authentication/Login";
 import "./App.css";
 import Signup from "./authentication/Signup";
+import PrivateRoute from "./authentication/PrivateRoute";
+import Home from "./pages/Home";
 
 function App() {
 	return (
 		<Router>
 			<AuthProvider>
 				<Switch>
+					{/* pages routes */}
+					<PrivateRoute path="/" component={Home} />
+
+					{/* Auth routes */}
 					<Route path="/login" component={Login} />
 					<Route path="/signup" component={Signup} />
 				</Switch>
